b877fcc92c0b9f6eb76caf28385b538fa66573ef
Leticia Valladares <112868363+leticiavalladares@users.noreply.github.com>
2025-06-23 22:43:00 +0200

    Add network policies exercices

--- test/http_app/app.py
+++ test/http_app/app.py
@@ -1,5 +1,22 @@
 from flask import Flask
+from prometheus_client import Counter, Histogram, generate_latest
+import time, random
+
 app = Flask(__name__)
+
+REQUEST_COUNT = Counter('api_requests_total', 'Total API Requests', ['method', 'endpoint'])
+REQUEST_LATENCY = Histogram('api_request_latency_seconds', 'Request latency', ['endpoint'])
+
+@app.route("/checkout")
+def checkout():
+    REQUEST_COUNT.labels(method='GET', endpoint='/checkout').inc()
+    with REQUEST_LATENCY.labels(endpoint='/checkout').time():
+        time.sleep(random.uniform(0.1, 0.5))
+        return "Checkout complete", 200
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {'Content-Type': 'text/plain'}
 
 @app.route("/health")
 def health():
